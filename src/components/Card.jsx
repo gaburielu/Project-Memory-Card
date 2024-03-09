ec79a51f9ef8e89285bd69cf6974e292e364cd5d
GabrielX <97820374+gaburielu@users.noreply.github.com>
2024-03-09 16:08:18 +0800

    try importing img in JSX

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/cards.css";
-import templateImg from "src/assets/Ghibli-card.png";
+import templateImg from "../assets/Ghibli-card.png";
 
 const Card = ({ gameFilms, handleCardClick, isFlipped }) => {
   return (
